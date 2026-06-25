7aefa40a02598710af2d6213c19ea04530ec753e
nhammen-kinkaid <nathaniel.hammen@kinkaid.org>
2026-06-25 11:28:34 -0500

    Initial commit

---
+++ functions.py
@@ -0,0 +1,8 @@
+def fahrenheit_to_celsius(fahrenheit):
+  return (fahrenheit - 32) * 5 / 9
+
+celsius = fahrenheit_to_celsius(90)
+
+print(fahrenheit_to_celsius(77))
+print(fahrenheit_to_celsius(95))
+print(fahrenheit_to_celsius(50))
---
+++ trace.py
@@ -0,0 +1,25 @@
+# First code tracing example
+index = 0
+while index<3:
+  index = index + 1
+  print(index)
+
+# Second code tracing example
+#index = 0
+#while index<3:
+#  print(index)
+#  index = index + 1
+
+# Third code tracing example
+#a = 5
+#b = 7
+#while b<10:
+#  b = b + 1
+#  print(b-a)
+
+# Fourth code tracing example
+#a = b = 0
+#while b-a >= 0:
+#  b = b + 1
+#  a = a + b - 1
+#  print(b-a)
